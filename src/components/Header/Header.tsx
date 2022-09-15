7dd6f8ffb5f460cf59bdec2d04ebf7bbb6a8568a
ahanshaw <angela.hanshaw@gmail.com>
2022-09-14 22:40:10 -0500

    add log out btn

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { auth } from '../../services/firebase';
+import { auth, logout } from '../../services/firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
 
 export function Header() {
@@ -9,11 +9,16 @@ export function Header() {
 			<p className="header-title">
 				<a href="/">Pilates Workout Planner</a>
 			</p>
+			{user && loading === false &&
+				<div className="header-admin">
+					<p><a className="link" href="/account/dashboard">Dashboard</a> <span className="divider">|</span> <button className="link" onClick={logout}>Log Out</button></p>
+				</div>
+			}
 			{!user && loading === false &&
 				<div className="header-admin">
 					<p><a className="link" href="/account/login">Log In</a> <span className="divider">|</span> <a className="link" href="/account/register">Register</a></p>
 				</div>
 			}
 		</header>
-    );
+	);
 }
